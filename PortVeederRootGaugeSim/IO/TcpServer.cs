caab73936f294cf6ca1720a8280018148f2e580f
Jurgen van Rooyen <68892316+JurgenvRooyen@users.noreply.github.com>
2020-08-24 14:26:51 +1200

    Added basic tcpserver and accompanying unittest (#33)
    * Added basic tcpserver and accompanying unittest
    
    * Switched Protocol Testing for Black Box testing
    
    * Fixed csproj
    
    * fixed csproj
    
    * Update azure-pipelines.yml
    
    * Update azure-pipelines.yml
    
    * Removed server testing from whitebox
    Fixed blackbox test

---
+++ PortVeederRootGaugeSim/IO/TcpServer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortVeederRootGaugeSim.IO
+{
+    class TcpServer
+    {
+        TcpListener listener;
+        // Dependecy injection for the protocol to call, possibly specify an interface for protocol to support multiple protocols?
+        TLS3XXProtocol protocol;
+        bool acceptIncoming;
+        Byte[] bytes;
+
+        public TcpServer(TLS3XXProtocol protocol)
+        {
+            Int32 port = 10001;
+            IPAddress addr = IPAddress.Parse("127.0.0.1");
+            this.protocol = protocol;
+            this.listener = new TcpListener(addr, port);
+            this.bytes = new Byte[256];
+        }
+
+        public void Start()
+        {
+            listener.Start();
+            acceptIncoming = true;
+            Listen();
+        }
+
+        private async void Listen()
+        {        
+            Console.WriteLine("Start listening");
+            while (acceptIncoming)
+            {
+                TcpClient client = await listener.AcceptTcpClientAsync();
+                HandleClient(client);
+            }
+        }
+
+        private async void HandleClient(TcpClient client)
+        {
+            NetworkStream nStream = client.GetStream();
+            byte[] buffer = new byte[256];
+            while ((await nStream.ReadAsync(buffer, 0, buffer.Length)) != 0)
+            {
+                string parsed = protocol.parse((System.Text.Encoding.ASCII.GetString(buffer)));
+                nStream.Write(System.Text.Encoding.ASCII.GetBytes(parsed));
+            }
+
+            nStream.Close();
+        }
+    }
+}
---
+++ PortVeederRootGaugeSimulatorBlackTesting/BlackBox.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using PortVeederRootGaugeSim.IO;
+using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace BlackBoxTest
+{
+    class BlackBox
+    {
+        TLS3XXProtocol protocol;
+        TcpServer server;
+        TcpClient client;
+
+        [SetUp]
+        public void SetUp()
+        {
+            protocol = new TLS3XXProtocol();
+            server = new TcpServer(protocol);
+            server.Start();
+            client = new TcpClient("127.0.0.1", 10001);
+        }
+
+        [Test]
+        public void Response()
+        {
+            Byte[] data = System.Text.Encoding.ASCII.GetBytes("test");
+            NetworkStream nStream = client.GetStream();
+            nStream.Write(data);
+
+            string response;
+            Int32 bytes = nStream.Read(data, 0, data.Length);
+            response = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+
+            nStream.Close();
+            Assert.IsNotNull(response);
+            Assert.AreNotEqual(response, String.Empty);
+            Assert.AreEqual(response, "\x02" + "9999" + "\x03");
+        }
+    }
+}
