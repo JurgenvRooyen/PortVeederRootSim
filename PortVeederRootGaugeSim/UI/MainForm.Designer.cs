8595caeb38cd053e3213e8c0101d4d2f44dd36a6
Vic Wassilieff <vicwassilieff123@gmail.com>
2020-09-23 17:05:47 +1200

    updated event handlers to work each time

--- PortVeederRootGaugeSim/UI/MainForm.Designer.cs
+++ PortVeederRootGaugeSim/UI/MainForm.Designer.cs
@@ -72,6 +72,7 @@
             this.ExitMenuItem.Name = "ExitMenuItem";
             this.ExitMenuItem.Size = new System.Drawing.Size(116, 26);
             this.ExitMenuItem.Text = "Exit";
+            this.ExitMenuItem.Click += new System.EventHandler(this.MenuItem_Click);
             // 
             // OptionsMenuItem
             // 
@@ -93,12 +94,14 @@
             this.EchoTestMenuItem.Name = "EchoTestMenuItem";
             this.EchoTestMenuItem.Size = new System.Drawing.Size(206, 26);
             this.EchoTestMenuItem.Text = "Echo Test";
+            this.EchoTestMenuItem.Click += new System.EventHandler(this.MenuItem_Click);
             // 
             // PortVeederRootMenuItem
             // 
             this.PortVeederRootMenuItem.Name = "PortVeederRootMenuItem";
             this.PortVeederRootMenuItem.Size = new System.Drawing.Size(206, 26);
             this.PortVeederRootMenuItem.Text = "Port Veeder-Root";
+            this.PortVeederRootMenuItem.Click += new System.EventHandler(this.MenuItem_Click);
             // 
             // HelpMenuItem
             // 
@@ -114,6 +117,7 @@
             this.AboutMenuItem.ShortcutKeys = System.Windows.Forms.Keys.F1;
             this.AboutMenuItem.Size = new System.Drawing.Size(157, 26);
             this.AboutMenuItem.Text = "About";
+            this.AboutMenuItem.Click += new System.EventHandler(this.MenuItem_Click);
             // 
             // flowLayoutPanel
             // 
